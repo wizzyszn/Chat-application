e39edb2ef3bdc902d72f89dec01fe814022feccc
wizzyszn <140736928+wizzyszn@users.noreply.github.com>
2024-05-09 08:32:34 +0100

    updated the server link

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:5000/api";
+export const baseUrl = "https://backend-chat-application.vercel.app/api";
 export const postRequest = async (url,body)=>{
     const response = await fetch(url,{
         method : 'POST',
